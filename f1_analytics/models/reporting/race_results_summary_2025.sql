663b74a3ffeaecd0d6dfa6985ad3244eebb2ca3d
Lucas Smith <lucasasmith@gmail.com>
2025-04-21 09:00:29 -0400

    Add summary stats for 2025 (#20)

---
+++ f1_analytics/models/reporting/race_results_summary_2025.sql
@@ -0,0 +1,17 @@
+-- Calculate the summary statistics for each driver for the season.
+
+select
+    rr.driver_id,
+    avg(rr.position) as avg_result,
+    min(rr.position) as best_result,
+    max(rr.position) as worst_result,
+    count(rr.position) as total_races
+from
+    {{ ref('stg_race_result') }} as rr
+    inner join {{ ref('stg_race') }} as race
+    using(race_id)
+where
+    rr.year = 2025
+    and rr.position is not null
+group by
+    all
