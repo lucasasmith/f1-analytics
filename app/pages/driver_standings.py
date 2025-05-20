0b7e37f542a3579e88e7c2bf8cf1e4d776b76932
Lucas Smith <lucasasmith@gmail.com>
2025-05-19 22:12:55 -0400

    Tweak standings page

--- app/pages/driver_standings.py
+++ app/pages/driver_standings.py
@@ -7,27 +7,24 @@ from pages import Option, issue_query
 st.set_page_config(layout="wide")
 st.markdown("# 2025 Driver Standings")
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     standings_query = """
-        select full_name, position, points
+        select driver_id, position, points
         from core.driver_standings order by position"""
     standings_df = issue_query(query_str=standings_query, return_obj=Option.POLARS)
     st.text("2025 Standings:")
-    st.dataframe(standings_df)
+    st.dataframe(standings_df, width=400)
 
 with col2:
-    results_query = "from reporting.race_results_2025"
-    results_df = issue_query(query_str=results_query, return_obj=Option.POLARS)
-    st.text("Result by race:")
-    st.dataframe(results_df.sort("race_desc", "position"))
-
-with col3:
     results_summary_query = "from reporting.race_results_summary_2025"
     results_summary_df = issue_query(query_str=results_summary_query, return_obj=Option.POLARS)
     st.text("Season results summary:")
-    st.dataframe(results_summary_df.sort("avg_result"))
+    st.dataframe(results_summary_df.sort("avg_result"), width=500)
 
+results_by_race_query = "from reporting.race_results_2025"
+results_by_race_df = issue_query(query_str=results_by_race_query, return_obj=Option.POLARS)
 st.text("Result by race:")
-st.line_chart(data=results_df, x="race_desc", y="position", color="driver_id", height=800)
+st.dataframe(results_by_race_df.sort("race_desc", "position"), use_container_width=True)
+st.line_chart(data=results_by_race_df, x="race_desc", y="position", color="driver_id", height=800)
