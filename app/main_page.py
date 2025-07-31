a6e408c75668ff7abb98a2e29c4575fa25a33b86
Lucas Smith <lucasasmith@gmail.com>
2025-07-31 08:57:37 -0400

    Improve descriptions (#29)
    * Fix typ0
    
    * Improve description
    
    * Ruff exclusions

--- app/main_page.py
+++ app/main_page.py
@@ -4,7 +4,7 @@ st.markdown("# F1 Analytics 🏎️")
 
 description = """This application is built utilizing Python, DuckDB, dbt, Polars, and Streamlit.
 More information and the code can be found [here](https://github.com/lucasasmith/f1-analytics). It
-is mainly a showcase for Data Engineering and having fun with data. Enjoy :smiley:.
+is mainly a showcase for Data and Analytics Engineering and simply having fun with data.
 
 Built by [Lucas Smith](www.linkedin.com/in/smithlucas)
 """
@@ -13,14 +13,12 @@ st.markdown(description)
 st.image("assets/f1_diagram.png")
 st.markdown("""
 
-    Data is first downloaded from GitHub and the [f1db](https://github.com/f1db/f1db) repo. CSV's
-    are extracted from the newest release and inserted into the existing dbt structure.
+    - Data is first downloaded from GitHub and the [f1db](https://github.com/f1db/f1db) repo.
+    - CSV's are extracted.
 
-    DuckDB is utilized to store the data.
-
-    dbt is used to:
-    - load the inital raw CSV's into the `raw` schema.
-    - create `core` [silver layer tables](https://github.com/lucasasmith/f1-analytics/tree/main/f1_analytics/models/core).
-    - create `reporting` [gold layer views](https://github.com/lucasasmith/f1-analytics/tree/main/f1_analytics/models/reporting).
-    - run data tests (verifying primary keys, ranges, etc.)
+    dbt:
+    - Seeds are utilized to build the bronze layer.
+    - Silver/core layer is built to clean and lightly transform the source data. [silver layer tables](https://github.com/lucasasmith/f1-analytics/tree/main/f1_analytics/models/core).
+    - Gold/reporting layer is used to provide easy to consume information and statistics. [gold layer views](https://github.com/lucasasmith/f1-analytics/tree/main/f1_analytics/models/reporting).
+    - Data tests are incorporate to find issues with the data (verifying primary keys, ranges, etc.).
 """)
